d1e63732404de7793bdbe6b2078f7cd433919f9f
Rams-dev <ramiroaltamirano.lpz@gmail.com>
2020-10-12 12:23:20 -0500

    prueba

--- Conexiones/Conexiones.go
+++ Conexiones/Conexiones.go
@@ -2,7 +2,7 @@ package conexiones
 
 const (
 	// MONGO_SERVER = "192.168.45.51"
-	MONGO_SERVER = "127.0.0.1"
+	MONGO_SERVER = "192.168.1.78"
 	MONGO_DB     = "SIVEM"
 	MONGO_DB_U   = "Usuarios"
 	MONGO_DB_CL  = "Clientes"
