022fec36514f5a27ba7d6b50412f62ecf10511b7
vadgun <vadgun@hotmail.com>
2021-01-17 14:02:38 -0600

    Hola

--- Conexiones/Conexiones.go
+++ Conexiones/Conexiones.go
@@ -1,8 +1,8 @@
 package conexiones
 
 const (
-	MONGO_SERVER = "192.168.1.77"
-	// MONGO_SERVER = "127.0.0.1"
+	// MONGO_SERVER = "192.168.1.77"
+	MONGO_SERVER = "127.0.0.1"
 	MONGO_DB     = "SIVEM"
 	MONGO_DB_U   = "Usuarios"
 	MONGO_DB_CL  = "Clientes"
